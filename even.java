e5f049bdf1497c99b212633e3ad09c5946433a2c
unknown <bachutey@gmail.com>
2024-08-07 06:26:31 +0300

    To check even or odd

---
+++ even.java
@@ -0,0 +1,11 @@
+public class even {
+    public static void main(String[] args){
+        int num=3;
+        if (num/2==0) {
+            System.out.println("Given Number is Eeven");
+        }
+        else{
+            System.out.println("Given Number is Odd");
+        }
+    }
+}
