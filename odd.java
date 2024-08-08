ad4d4746685d7b4d02824200f58a0dc9fcc47984
unknown <bachutey@gmail.com>
2024-08-08 06:18:25 +0300

    odd numbers

---
+++ odd.java
@@ -0,0 +1,11 @@
+public class odd {
+    public static void main(String[] args){
+      int num=3;
+      if(num/2!=0){
+        System.out.println("Given Number "+num+" is odd ");
+      }
+      else{
+        System.out.println("Given Number "+num+" is Even ");
+      }
+    }
+}
