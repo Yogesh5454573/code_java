99efc38f2a97d9581ecbf0e86297bf8b1f77b910
unknown <bachutey@gmail.com>
2024-08-08 06:11:49 +0300

    Swapping A numbers

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap {
+   public static void main(String[] args){
+    int a,b;
+    a=10;
+    b=20;
+    System.out.println("The Value of a: "+a+" Value of b: "+b);
+    b=b-a;
+    a=b+a;
+    System.out.println("After Swapping Value of a: "+a+" Value of b: "+b);
+   }
+}
