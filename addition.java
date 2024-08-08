0389aad696b475e0606a717ea710aac894edeff6
unknown <bachutey@gmail.com>
2024-08-08 05:58:26 +0300

    Basic Operation

---
+++ addition.java
@@ -0,0 +1,18 @@
+public class addition {
+    public static void main(String[] args){
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println("Addition Of Two Numbers is "+c);
+        c=a-b;
+        System.out.println("Substraction Of Two Numbers is "+c);
+        c=a*b;
+        System.out.println("Multiplication Of Two Numbers is "+c);
+        c=a/b;
+        System.out.println("Division of Two Number is "+c);
+        c=a%b;
+        System.out.println("Reminder is "+c);
+        
+    }
+}
