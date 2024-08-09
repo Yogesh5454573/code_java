c6af20a809ebe04d130ec26a7eacf9ceac55414a
unknown <bachutey@gmail.com>
2024-08-09 06:39:49 +0300

    while loop

---
+++ loops.java
@@ -0,0 +1,10 @@
+public class loops {
+    public static void main(String[] args){
+        int a=1;
+        while (a<=100) {
+            System.out.print("\t"+a);
+            a++;
+        }
+        
+    }
+}
