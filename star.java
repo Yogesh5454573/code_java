ccdd20b9dab1c044c22cbb6395856d8b25ce7bbf
unknown <bachutey@gmail.com>
2024-08-10 07:05:29 +0300

    Simple Pattern

---
+++ star.java
@@ -0,0 +1,11 @@
+public class star {
+    public static void main(String[] args){
+        int i,j,num=10;
+        for(i=1;i<=num;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+    }
+}
