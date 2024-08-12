7a170d84ef292a524073107aea42066af8aa62ac
unknown <bachutey@gmail.com>
2024-08-12 06:27:19 +0300

    hello world code

---
+++ epattern.java
@@ -0,0 +1,21 @@
+public class epattern {
+    public static void main(String[] args){
+        int i,j;
+        for(i=1;i<=4;i++){
+            System.out.print("*");
+        }
+        for(j=1;j<=4;j++){
+            System.out.println("*");
+        }
+        for(i=1;i<=4;i++){
+            System.out.print("*");
+        }
+        for(j=1;j<=4;j++){
+            System.out.println("*");
+        }
+        for(i=1;i<=5;i++){
+            System.out.print("*");
+        }
+
+    }
+}
