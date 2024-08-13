62371ff89103b037be12714c58b4e27b7d4c860b
unknown <bachutey@gmail.com>
2024-08-13 18:06:21 +0300

    Table Of 1 to 30

---
+++ table.java
@@ -0,0 +1,13 @@
+public class table{
+    public static void main(String[] args){
+        int i,j,k;
+        for(i=1;i<=10;i++){
+          for(k=1;k<=30;k++){
+            j=i*k;
+            System.out.print(j+" ");
+          }
+          System.out.println("\t");
+           
+        }
+    }
+}
